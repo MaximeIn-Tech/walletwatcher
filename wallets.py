5925ef80699f40ffbccbd5df26bd2bc52d09a4ca
Tech Sherpa <119629208+ItsTechSherpa@users.noreply.github.com>
2024-05-05 20:00:02 +0200

    Add the skeleton of the alerts check

--- wallets.py
+++ wallets.py
@@ -1,13 +1,151 @@
-from dataclasses import dataclass
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+############## THETA BLOCKCHAIN #############
+# TODO: Limiter les calls pour Thetascan à 2 par secondes
 
 
-@dataclass
-class Wallet:
-    wallet_name: str
-    wallet_address: str
-    contract_address: str
-    token_symbol: str
-    trigger: float
-    balance: float
-    stake: bool
-    user_id: int
+async def fetch_data_contract(contract_address):
+    url = f"https://www.thetascan.io/api/contract/?contract={contract_address}"
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+async def fetch_theta_single_wallet_balance(wallet_address, contract_address):
+    url = f"http://www.thetascan.io/api/contract/?contract={contract_address}&address={wallet_address}"
+
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+async def fetch_theta_stake(wallet_address):
+    url = f"https://explorer-api.thetatoken.org/api/stake/{wallet_address}?types[]=vcp&types[]=gcp&types[]=eenp"
+
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+############## ETHEREUM BLOCKCHAIN #############
+ethapikey = os.getenv("ETH_API_KEY")
+
+
+async def fetch_eth_balance_multiple_addresses(wallet_addresses):
+    url = f"https://api.bscscan.com/api?module=account&action=balancemulti&address={wallet_addresses}&tag=latest&apikey={ethapikey}"
+
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+async def fetch_eth_single_wallet_balance(wallet_addresse, contract_address):
+
+    url = f"https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress={contract_address}&address={wallet_addresse}&tag=latest&apikey={ethapikey}"
+
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+############## BSC BLOCKCHAIN #############
+
+bscapikey = os.getenv("BSC_API_KEY")
+
+
+async def fetch_bsc_single_wallet_balance(wallet_addresse, contract_address):
+    url = f"https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress={contract_address}&address={wallet_addresse}&tag=latest&apikey={bscapikey}"
+
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+async def fetch_bnb_balance_multiple_addresses(wallet_addresses):
+    url = f"https://api.etherscan.io/api?module=account&action=balancemulti&address={wallet_addresses}&tag=latest&apikey={bscapikey}"
+
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+        else:
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
